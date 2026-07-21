ddca74793cceee4c40070704b9f3b457690ff5b8
Jorge Ivan Belalcazar <jorge.belalcazar@correounivalle.edu.co>
2026-07-21 14:26:43 -0500

    feat: add Orientation enum for ship placement direction

---
+++ src/main/java/com/example/batallanaval/model/Orientation.java
@@ -0,0 +1,14 @@
+package com.example.batallanaval.model;
+
+/**
+ * The orientation in which a ship can be placed on the board (see HU-1).
+ *
+ */
+public enum Orientation {
+
+    /** The ship extends to the right, sharing the same row. */
+    HORIZONTAL,
+
+    /** The ship extends downward, sharing the same column. */
+    VERTICAL
+}
