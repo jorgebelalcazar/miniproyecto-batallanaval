b5f31d8ccb6a58b0f2471bb46c539af8931af6a0
Jorge Ivan Belalcazar <jorge.belalcazar@correounivalle.edu.co>
2026-07-22 21:43:01 -0500

    docs: add generated Javadoc HTML documentation

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.batallanaval","l":"com.example.batallanaval"},{"m":"com.example.batallanaval","l":"com.example.batallanaval.controller"},{"m":"com.example.batallanaval","l":"com.example.batallanaval.view"}];updateSearchResults();
